59e1ade231010637b1c83cda304c809b048d96b2
Daniel Schwabacher <danielschwabacher@gmail.com>
2016-12-04 23:59:22 -0700

    updated tests

--- tests/crawler_tests.py
+++ tests/crawler_tests.py
@@ -2,12 +2,12 @@ import unittest
 from cranberry import crawler
 
 class TestCrawler(unittest.TestCase):
-    def test_output_dir_validation(self):
-        test_crawler = crawler.Crawler("outputDir", "test")
-        self.assertEqual(test_crawler.output_directory, "outputDir", "FAILED -- output dir assignment in Crawler.")
-    def test_song_file_validation(self):
-        test_crawler = crawler.Crawler("test", "songFileLocation")
-        self.assertEqual(test_crawler.song_loc, "songFileLocation", "FAILED -- song file assignment in Crawler.")
+	def test_output_dir_validation(self):
+		test_crawler = crawler.Crawler("outputDir", "test")
+		self.assertEqual(test_crawler.output_directory, "outputDir", "FAILED -- output dir assignment in Crawler.")
+	def test_song_file_validation(self):
+		test_crawler = crawler.Crawler("test", "songFileLocation")
+		self.assertEqual(test_crawler.song_loc, "songFileLocation", "FAILED -- song file assignment in Crawler.")
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
