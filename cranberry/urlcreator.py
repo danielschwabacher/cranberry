500ce2931bc8d836406769fc1be9c3aa59631b14
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-11-15 20:38:38 -0700

    changes to way headers are passed. Hopefully resolves some CAPTCHA issues.

--- cranberry/urlcreator.py
+++ cranberry/urlcreator.py
@@ -5,6 +5,8 @@ import re
 import logging
 import time
 import random
+from fake_useragent import UserAgent
+
 class URL_Creator():
 	""" The URL_Creator class is used to transform the song-list created by Linter into
 	a form usable by the youtube-dl Uploader. It handles stuff like the URL transformations. """
@@ -14,16 +16,22 @@ class URL_Creator():
     		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 		}
 
+	def update_headers(self):
+		ua = UserAgent()
+		self.cranberry_fake_headers['User-Agent'] = ua.random
+		logging.debug("headers are: " + str(self.cranberry_fake_headers))
+
 	def create_urls(self):
 		list_of_urls = []
 		for item in self.songs_list:
 			print("Getting url for song: ", item)
+			self.update_headers()
 			html_content = requests.get("http://www.youtube.com/results?search_query=" + item, headers=self.cranberry_fake_headers)
 			try:
 				search_results = re.findall(r'\/watch\?v=(.{11})', html_content.content.decode('utf-8'))	
 				logging.debug("Search results are: " + str(search_results))
 				# This may help reduce captcha errors.
-				# time.sleep(random.random() * 3)
+				time.sleep(random.random() * 3)
 				if html_content.status_code == 503:
 					print("CAPTCHA Error, cranberry cannot continue. Run with -v for more information. Cranberry will now exit.")
 					logging.debug("Status code: ", html_content.status_code)
@@ -31,7 +39,7 @@ class URL_Creator():
 					logging.debug("Youtube has detected that we have requested too many resources. This usually happens when you try to download many songs in a short period of time.")
 					logging.debug("To fix this, you can manually complete the CAPTCHA on Youtube, or you can wait a bit and try using cranberry again.")
 					exit(-1)
-				url_string = "http://www.youtube.com/watch?v=" + search_result[0]
+				url_string = "http://www.youtube.com/watch?v=" + search_results[0]
 				list_of_urls.append(url_string)
 			except Exception as e:
 				print("Unknown error, skipping: " + str(item))
