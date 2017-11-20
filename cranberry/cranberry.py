0a2069db8e146d8b527835e9530c0ab44b36ad52
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-11-20 00:31:36 -0500

    added testing argument.

--- cranberry/cranberry.py
+++ cranberry/cranberry.py
@@ -15,7 +15,7 @@ class Cranberry():
 	also be thought of as runners. Cranberry objects delegate the bulk of their
 	work to other modules (filesystem, parser, uploader, etc.) to do the associative task.
 	There should not be more than one of these objects in any given program using Cranberry. """
-	def __init__(self, dl_dir, songs_loc, verbose, quiet, auto_append, time):
+	def __init__(self, dl_dir, songs_loc, verbose, quiet, auto_append, time, display_info_exit):
 		self.dl_dir = dl_dir
 		self.songs_loc = songs_loc
 		self.verbose = verbose
@@ -26,16 +26,27 @@ class Cranberry():
 		self.sensor = sensor
 		self.auto_append = auto_append
 		self.request_delay_time = time
+		self.show_and_exit = display_info_exit
 
 	def print_args(self):
 		print("Download dir: ", self.dl_dir)
 		print("Source File: ", self.songs_loc)
 
 	def run(self):
+		if (self.show_and_exit):
+			# Only display cranberry session info, then exit.
+			print("-----Cranberry Session Info-----")
+			print("1.) Output directory: " + str(self.dl_dir))
+			print("2.) Song file: " + str(self.songs_loc))
+			print("3.) Quiet mode: " + str(self.quiet))
+			print("4.) Sensor: " + str(self.sensor))
+			print("5.) Append Audio: " + str(self.auto_append))
+			print("6.) Request Delay Time: " + str(self.request_delay_time))
+			exit(0)
+		# ~~~~~~~~~~
 		print("---WELCOME TO CRANBERRY---")
 		filesystem_crawler = crawler.Crawler(self.dl_dir, self.songs_loc)
 		filesystem_crawler.operations_runner()
-		# ~~~~~~~~~~
 		if (self.auto_append == True):
 			song_file_parser = linter.Linter(self.songs_loc, True)
 			final_songs_list = song_file_parser.create_final_list()
