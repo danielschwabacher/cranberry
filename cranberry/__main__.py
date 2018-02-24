0d377dde34c8cee43cc20223f4c66b28950e93f2
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-02-24 00:25:08 -0700

    change default behavior with no arguments provided to just display help and then exit.

--- cranberry/__main__.py
+++ cranberry/__main__.py
@@ -6,12 +6,10 @@ from cranberry import cranberry
 
 def main(args=None):
 	"""Entry point for cranberry."""
-
 	# If output and source flags are not specified -- use these
 	DOWNLOAD_DIR = os.path.expanduser('~') + '/CranberryMusic/'
 	SONGS_LOCATION = os.path.expanduser('~') + '/songsList.txt'
 	DEFAULT_SENSOR = 'default'
-
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-o',"--output-directory", default=DOWNLOAD_DIR, help='Specify directory to store downloaded files. Defaults to ~/CranberryMusic/')
 	parser.add_argument('-s',"--source-file", default=SONGS_LOCATION, help='Specify plaintext that contains names of songs to download. Defaults to ~/songList.txt')
@@ -23,8 +21,12 @@ def main(args=None):
 	parser.add_argument('-x', "--sensor", default=DEFAULT_SENSOR, help="Advanced: specify a custom sensor to use during the parsing phase. Not yet implemented, option ignored.")
 	args = parser.parse_args()
 	args_dict = vars(args)
-	cranberry_main = cranberry.Cranberry(args_dict['output_directory'], args_dict['source_file'], args_dict['verbose'], args_dict['quiet'], args_dict['append_audio'], args_dict['request_delay_time'], args_dict['display_session_info'])
-	cranberry_main.run()
+	if (args_dict['source_file'] == SONGS_LOCATION and args_dict['output_directory'] == DOWNLOAD_DIR and args_dict['sensor'] == DEFAULT_SENSOR):
+		parser.print_help()
+		exit(0)
+	else:
+		cranberry_main = cranberry.Cranberry(args_dict['output_directory'], args_dict['source_file'], args_dict['verbose'], args_dict['quiet'], args_dict['append_audio'], args_dict['request_delay_time'], args_dict['display_session_info'])
+		cranberry_main.run()
 
 if __name__ == "__main__":
 	main()
