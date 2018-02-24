a0001b95eadd5c41b428e740c0bcc8750144571e
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-02-24 00:58:37 -0700

    more readable errors when song files do not exist.

--- cranberry/crawler.py
+++ cranberry/crawler.py
@@ -7,28 +7,31 @@ class Crawler():
 	def __init__(self, dir_c, file_c):
 		self.output_directory = dir_c
 		self.song_loc = file_c
+
 	def validate_ouput_dir(self):
 		if (os.path.exists(self.output_directory) == True):
 			return 0
 		else:
 			logging.debug("The output directory does not exist, creating it...")
 			self.create_output_dir(self.output_directory)
+
 	def validate_song_file(self):
-		if (os.path.exists(self.song_loc) == True):
+		if (os.path.exists(self.song_loc)):
 			return 0
 		else:
-			logging.debug("Song file does not exists, create a song file before continuing.")
-
+			print("Song file does not exist, create the song file before continuing.")
+			logging.debug("Song file {0} does not exist, you must create it before continuing".format(self.song_loc))
+			exit(-1)
+			
 	def create_output_dir(self):
 		try:
 			os.mkdir(self.output_directory)
 		except:
 			logging.debug("Error creating directory at: ", self.output_directory)
+			exit(-1)
 
 	# keep Cranberry objects clean by implementing logic in one function within Crawler
 	def operations_runner(self):
-		try:
-			self.validate_ouput_dir(self.output_directory)
-			self.validate_song_file(self.song_loc)
-		except:
-			logging.debug("Filesystem checks failed")
+		self.validate_ouput_dir()
+		self.validate_song_file()
+
